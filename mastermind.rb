e296bcf100d7eca163ae5d9b1bb136787e511e91
Alex Kara <190240100+akarathanassis@users.noreply.github.com>
2025-01-28 23:53:39 +1100

    added skeleton code to Game class

---
+++ mastermind.rb
@@ -0,0 +1,30 @@
+module Mastermind
+
+
+
+
+  class Game 
+    def initialize(player)
+      # define a 4 element array to store secret code - @code = Array.new(4)
+      # define the player 
+      
+    end
+
+
+
+    def play_game
+      # generate random 4 color code 
+      loop do # this needs to loop 12 times for the 12 rounds of guessing 
+        # player makes a guess 
+        # 
+        # check if the guess was correct 
+        # 
+        # computer gives feedback on guess
+        # 
+      end
+      # if 12 rounds are done and game has not finished yet, then player loses
+    end
+
+
+  end
+end
